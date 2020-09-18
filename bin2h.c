17067f7c50fb426b14527f7f2635ee921e59a992
Elviss Strazdins <elviss@elviss.lv>
2020-09-18 05:32:43 +0300

    Clean up the help output

--- bin2h.c
+++ bin2h.c
@@ -4,16 +4,16 @@
 
 static void print_help(const char* name)
 {
-    printf("OVERVIEW: Converts binary file to C header\n\n");
-    printf("USAGE: %s -i <input> -o <output> -n <name> -d -z\n\n", name);
-    printf("OPTIONS:\n");
-    printf("	-d				Print decimal instead of hex literals\n");
-    printf("	-e				Output variable that holds a pointer to the last element of the data\n");
-    printf("	-i <input>			Specify input file\n");
-    printf("	-n <name>			Specify variable name\n");
-    printf("	-o <output>			Specify output file\n");
-    printf("	-s				Output variable for data size\n");
-    printf("	-z				Add zero to the end of the array\n");
+    fprintf(stdout, "OVERVIEW: Converts binary file to C header\n\n");
+    fprintf(stdout, "USAGE: %s -i <input> -o <output> -n <name> -d -e -s -z\n\n", name);
+    fprintf(stdout, "OPTIONS:\n"
+        "	-d				Print decimal instead of hex literals\n"
+        "	-e				Output variable that holds a pointer to the last element of the data\n"
+        "	-i <input>		Specify input file\n"
+        "	-n <name>		Specify variable name\n"
+        "	-o <output>		Specify output file\n"
+        "	-s				Output variable for data size\n"
+        "	-z				Add zero to the end of the array\n");
 }
 
 int main(int argc, char *argv[])
